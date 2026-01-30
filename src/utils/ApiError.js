4d748c75206e06d57689fc9d609f8cd649ab5074
PrabhatS <prabhat.sharma@hashstudioz.com>
2026-01-30 11:46:00 +0530

    fix issues of apiError

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -7,7 +7,15 @@ class ApiError extends Error{
     ){
         super(message)  //jab bhi me hum override karte h to super call krte h toh yha par humne super ke andar message send krdiya 
         this.statusCode=statusCode
-
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=error
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
     }
 }
 export {ApiError};
