a75d3bfaffd19ef0b2ee9ddd5bb120782ec09604
PrabhatS <prabhat.sharma@hashstudioz.com>
2026-02-04 17:48:54 +0530

    create user register api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,80 @@
-import { json } from "express";
 import {asyncHandler} from "../utils/asyncHandler.js";
-
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser=asyncHandler(async(req,res)=>{
 
-    console.log("hereres")
-res.status(200).json({
-  message: "Ok"
-});     
+    // steps to register user
+    // 1- get details from user (frontend)
+    // 2- validate details 
+    //3- check if user already register or not 
+    //4-check for images and avtar 
+    // 5- upload them to cloudinary 
+    // 6- create user object - create entry in db 
+    // 7- remove password and refresh token from response
+    //8- check for user creation
+    //9- return response
+
+    const{fullName,email,username,password}=req.body;
+    // console.log("FullName is+++++++++ ",fullName);
+
+    if([fullName,email,username,password].some((field)=>field?.trim()==="")){
+        throw new ApiError(404,"All fields are required")
+    }
+
+// check out if user existed in DB or not
+
+const userExits=User.findOne({
+    $or:[{username},{email}]
 })
+
+if(userExits){
+    throw new ApiError(409,"User with email or username already Exist")
+}
+
+
+// now we will check if avtar and coverImages exist on server or not using multer 
+// like req.body multer gives us a files method to get file path
+
+const avtarFilePath=req.files?.avatar[0].path;
+const coverImageFilePath=req.files?.avatar[0].path;
+
+if(!avtarFilePath) throw new ApiError(400,"Avtar is required")
+console.log("AVtar local path is",avtarFilePath);
+
+
+if(!coverImageFilePath) throw new ApiError(400,"CoverImage is required")
+console.log("CoverImage local path is",coverImageFilePath);
+
+
+
+
+const avtar=await uploadOnCloudinary(avtarFilePath);
+const coverImage=await uploadOnCloudinary(coverImageFilePath);
+
+if(!avtar) throw new ApiError(400,"Avtar is required");
+
+// now we will cerate a user in db
+
+const user=User.create({
+    fullName,
+    avtar:avtar.url,
+    coverImage:coverImage?.url||"",
+    email,
+    password,
+    username:username.toLowerCase()
+})
+
+const createdUser=await User.findById(user._id).select("-password -refreshToken")
+if(!createdUser) throw new ApiError(500,"Something went wrong while registering user")
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
+})
+
+
+
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router=Router();
 
-router.route("/register").post(registerUser)
+//here we have injected upload middleware (upload) to upload coverimage and avtar image
+
+router.route("/register").post(upload.fields([
+    {
+        name:'avtar', //this name feild should be same in frontend and backend
+        maxCount:1
+    },
+    {
+        name:'coverImage',
+        maxCount:1
+    }
+]),registerUser)
 
 export default router;
