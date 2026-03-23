c9e2f74aae5332626851d67e4afffd78f75bd384
akumar <prabhatpandit1651@gmail.com>
2026-03-23 15:00:50 +0530

    Added api for update avatar and coverImage

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema=new Schema({
         trim:true,
         index:true
     },
-     avtar:{
+     avatar:{
         type:String, //cloudinay url will be stored here
         required:true,
     },
